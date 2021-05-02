8f0a0c2bf167a4ac86376c1a245d197f4e8ff30e
Umar Ahmed T <tumarahmed@gmail.com>
2021-05-02 18:18:00 +0530

    gitIgnore deleted

---
+++ out/production/Attendance Manager/com/attendance_manager/components/SQL.sql
@@ -0,0 +1,39 @@
+create database javaproject;
+use javaproject;
+create table user(email varchar(50),password varchar(50));
+
+select * from user;
+ALTER TABLE user
+    ADD PRIMARY KEY (email);
+
+
+create table subject(subjectCode varchar(50),staff varchar(50));
+ALTER TABLE subject
+    ADD PRIMARY KEY (subjectCode,staff);
+
+
+select * from user;
+create table registers(email varchar(50),attendedClasses int,missedClasses int,subjectName varchar(50),
+subjectCode varchar(50),staff varchar(50));
+ALTER TABLE registers
+    ADD FOREIGN KEY (email) REFERENCES user(email);
+
+ALTER TABLE registers
+    ADD FOREIGN KEY (subjectCode,staff) REFERENCES subject(subjectCode,staff);
+
+
+create table history(subjectCode varchar(50),email varchar(50),attendance int);
+ALTER TABLE history
+    ADD FOREIGN KEY (subjectCode) REFERENCES subject(subjectCode);
+ALTER TABLE history
+    ADD FOREIGN KEY (email) REFERENCES user(email);
+
+
+
+
+
+
+
+
+
+
